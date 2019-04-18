a352c8d84b3e27756b3f2a74c22f5e3bb3610831
Jeremy Andrews <jeremy@tag1consulting.com>
2019-04-18 08:35:53 +0200

    implement Part 2

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,60 @@ struct InputBuffer {
     input_length: usize,
 }
 
+enum MetaCommandResult {
+    MetaCommandSuccess,
+    MetaCommandUnrecognizedCommand,
+}
+
+enum PrepareResult {
+    PrepareSuccess,
+    PepareUnrecognizedStatement,
+}
+
+enum StatementType {
+    None,
+    StatementInsert,
+    StatementSelect,
+}
+
+struct Statement {
+    stype: StatementType,
+}
+
+fn do_meta_command(input_buffer: &InputBuffer) -> MetaCommandResult {
+    if input_buffer.buffer == ".exit" {
+        std::process::exit(0);
+    } else {
+        MetaCommandResult::MetaCommandUnrecognizedCommand
+    }
+}
+
+fn prepare_statement(input_buffer: &InputBuffer, statement: &mut Statement) -> PrepareResult {
+    if input_buffer.buffer.starts_with("insert") {
+        statement.stype = StatementType::StatementInsert;
+        PrepareResult::PrepareSuccess
+    } else if input_buffer.buffer == "select" {
+        statement.stype = StatementType::StatementSelect;
+        PrepareResult::PrepareSuccess
+    } else {
+        PrepareResult::PepareUnrecognizedStatement
+    }
+}
+
+fn execute_statement(statement: &Statement) {
+    match statement.stype {
+        StatementType::None => {
+            println!("This is an uninitialized statement.");
+        }
+        StatementType::StatementInsert => {
+            println!("This is where we would do an insert.");
+        },
+        StatementType::StatementSelect => {
+            println!("This is where we would do a select.");
+        }
+    }
+}
+
 fn print_prompt() {
     print!("rusql> ");
     io::stdout().flush().unwrap();
@@ -36,10 +90,27 @@ fn main() {
     loop {
         print_prompt();
         input_buffer = read_input(input_buffer);
-        if input_buffer.buffer == ".exit" {
-            std::process::exit(0);
-        } else {
-            println!("Unrecognized command '{}'.\n", input_buffer.buffer);
+        if input_buffer.buffer.chars().next().unwrap() == '.' {
+            match do_meta_command(&input_buffer) {
+                MetaCommandResult::MetaCommandSuccess => {},
+                MetaCommandResult::MetaCommandUnrecognizedCommand => {
+                    println!("Unrecognized keyword at start of '{}'.", input_buffer.buffer);
+                },
+            }
+        }
+        else {
+            let mut statement = Statement {
+                stype: StatementType::None,
+            };
+            match prepare_statement(&input_buffer, &mut statement) {
+                PrepareResult::PrepareSuccess => {
+                    execute_statement(&statement);
+                    println!("Executed.");
+                }
+                PrepareResult::PepareUnrecognizedStatement => {
+                    println!("Unrecognized keyword at start of '{}'.", input_buffer.buffer);
+                }
+            }
         }
     }
 }
